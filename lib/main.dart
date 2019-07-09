2ceebf514a500c65ecc7b6067302f8077abc0ad6
muneer <muneer@webcastle.in>
2019-07-09 14:26:53 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,34 @@ class MapSampleState extends State<MapSample> {
     zoom: 14.4746,
   );
 
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  void setUpMarker() {
+    final MarkerId markerId = MarkerId("marker_id_1");
+    final Marker marker = Marker(
+        markerId: markerId,
+        consumeTapEvents: false,
+        position: LatLng(37.42796133580664, -122.085749655962),
+        infoWindow: InfoWindow(title: 'Just Text'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure));
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setUpMarker();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
+        markers: Set<Marker>.of(markers.values),
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
